00eca201e1a4e172447b52741df986dfde4f4844
Igor <igor_sazonov@bk.ru>
2015-05-28 19:13:24 +0300

    main layout improvements

--- app/src/main/java/com/hse/ndolgopolov/thermostat/MainActivity.java
+++ app/src/main/java/com/hse/ndolgopolov/thermostat/MainActivity.java
@@ -1,10 +1,15 @@
 package com.hse.ndolgopolov.thermostat;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.lang.reflect.Field;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,14 +21,37 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        TextView currTemp = (TextView) findViewById(R.id.currentTemperatureTextView);
+        TextView currTempLabel = (TextView) findViewById(R.id.currentTemperatureLabelTextView);
+        TextView targetTemp = (TextView) findViewById(R.id.targetTemperatureTextView);
+        TextView schedule = (TextView) findViewById(R.id.scheduleLabelTextView);
+        TextView toolbarTextView = getActionBarTextView(mToolbar);
+
+        Typeface roboto_light = Typeface.createFromAsset(this.getAssets(), "Roboto-Light.ttf");
+        Typeface roboto_bold = Typeface.createFromAsset(this.getAssets(), "Roboto-Bold.ttf");
+
+        currTemp.setTypeface(roboto_light);
+        currTempLabel.setTypeface(roboto_light);
+        targetTemp.setTypeface(roboto_light);
+        schedule.setTypeface(roboto_light);
+        toolbarTextView.setTypeface(roboto_bold);
+        toolbarTextView.setTextSize(20);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -33,11 +61,24 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        // Calendar icon clicked
+        if (id == R.id.action_calendar) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private TextView getActionBarTextView(Toolbar toolbar) {
+        TextView titleTextView = null;
+
+        try {
+            Field f = toolbar.getClass().getDeclaredField("mTitleTextView");
+            f.setAccessible(true);
+            titleTextView = (TextView) f.get(toolbar);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+        }
+
+        return titleTextView;
     }
 }
